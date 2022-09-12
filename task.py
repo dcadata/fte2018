1c3bfcd38f39c4958da96a6382f163d71eda6999
Devon Ankar <devon.ankar@gmail.com>
2022-09-12 15:59:02 -0400

    Rename outputs

--- task.py
+++ task.py
@@ -159,10 +159,10 @@ def _combine_forecast_and_election_results(chamber: str, use_today: bool = True,
 
 def main() -> None:
     params = {
-        'GOVERNORS - model launch': dict(chamber='governor', use_today=False, fcst_date=(2022, 10, 11)),
-        'SENATE - this day in 2018': dict(chamber='senate'),
-        'GOVERNORS - closest to election': dict(chamber='governor', use_today=False),
-        'SENATE - closest to election': dict(chamber='senate', use_today=False),
+        'GOVERNORS - 1 model launch': dict(chamber='governor', use_today=False, fcst_date=(2022, 10, 11)),
+        'GOVERNORS - 2 closest to election': dict(chamber='governor', use_today=False),
+        'SENATE - 1 this day in 2018': dict(chamber='senate'),
+        'SENATE - 2 closest to election': dict(chamber='senate', use_today=False),
     }
     for label, i in params.items():
         _combine_forecast_and_election_results(**i).to_csv(f'outputs/{label}.csv', index=False)
